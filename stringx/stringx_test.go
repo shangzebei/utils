13248b9e1aedc3ea0c63463b766ee3b4a99acb9b
shang <917058965@qq.com>
2019-06-26 13:53:21 +0800

    add test

--- stringx/stringx_test.go
+++ stringx/stringx_test.go
@@ -8,9 +8,10 @@ import (
 )
 
 func TestName(t *testing.T) {
-	fmt.Println(Distinct([]string{"ehe", "ehe", "tsh", "tsh", "walletapi", "walletapi", "gateway", "gateway", "walletapi"}))
-	fmt.Println(Distinct([]string{"ehe", "walletapi", "walletapi"}))
 	assert.Equal(t, []string{"ehe", "walletapi"}, Distinct([]string{"ehe", "walletapi", "walletapi"}))
+	assert.Equal(t, []string{"ehe", "walletapi"}, Distinct([]string{"ehe", "ehe", "walletapi"}))
+	assert.Equal(t, []string{"ehe", "walletapi"}, Distinct([]string{"ehe", "walletapi", "ehe"}))
+	assert.Equal(t, []string{"ehe", "gateway", "tsh", "walletapi"}, Distinct([]string{"ehe", "ehe", "tsh", "tsh", "walletapi", "walletapi", "gateway", "gateway", "walletapi"}))
 }
 
 func TestHexTODecimal(t *testing.T) {
